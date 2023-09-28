db30ff5101b378e16946dac438575efdae9bc8be
shubham kolape <107977222+shubhamkolape@users.noreply.github.com>
2023-09-28 12:30:14 +0530

    edited VIte file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/Todo-with-Context-API.git/" ,
+  base : "/Todo-with-Context-API" ,
   plugins: [react()],
 })
